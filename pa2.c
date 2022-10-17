27c9c9ff4eb263de03ff3cf4c82589a16ae95b74
Eduardo Ayerve Cruz | 에두아르도 <eduardo.ayerve@gmail.com>
2022-10-17 23:07:44 +0900

    Update pa2.c

--- pa2.c
+++ pa2.c
@@ -2,7 +2,7 @@
 // 
 //   4190.308 Computer Architecture (Fall 2022)
 // 
-//   Project #2: SFP16 (16-bit floating point) Adder
+//   Project #2: SFP16 (16-bit ing po) Adder
 // 
 //   October 4, 2022
 // 
@@ -23,6 +23,8 @@ typedef unsigned short SFP16;
 
 SFP16 E = 0;
 SFP16 res_denormal = 0;
+SFP16 res_normal = 0;
+SFP16 res_zero = 0;
 
 SFP16 sign(x){return x >> 15;}
 
@@ -57,7 +59,11 @@ SFP16 modifyBit(SFP16 bit_string, SFP16 position, SFP16 new_value)
 SFP16 shift_right_mantissa(SFP16 mantissa, SFP16 oprd){
   if(oprd == 0) return mantissa;
   else{
-    if (mantissa % 2 == 1) mantissa = (mantissa >> 1) + 1;
+    if (mantissa % 2 == 1){
+      SFP16 or_value = ((mantissa << 15) >> 15) || ((mantissa << 14) >> 15);
+      mantissa = (mantissa >> 1);
+      mantissa = modifyBit(mantissa, 0, or_value);
+    }
     else mantissa = (mantissa >> 1);
     return shift_right_mantissa(mantissa, oprd - 1);
   }
@@ -119,7 +125,9 @@ SFP16 round_mantissa(SFP16 mantissa, SFP16 expectedBits){
   }
   // Round
   if(mantissa % 8 == 0) {
+    //if(!res_denormal){
     E--;
+    //}
     return mantissa >> 2;
   }
   if(mantissa % 8 == 1) return mantissa >> 2;
@@ -127,11 +135,11 @@ SFP16 round_mantissa(SFP16 mantissa, SFP16 expectedBits){
   if(mantissa % 8 == 3) return shift_right_mantissa(mantissa, 2);
   if(mantissa % 8 == 4) return mantissa >> 2;
   if(mantissa % 8 == 5) return mantissa >> 2;
-  if(mantissa % 8 == 6) return (shift_right_mantissa(mantissa, 3)) << 1;
+  if(mantissa % 8 == 6) return (shift_right_mantissa(mantissa + 2, 3)) << 1;
   if(mantissa % 8 == 7) {
     // The last three numbers are 0
-    E++;
-    return (mantissa + 1) >> 3;
+    // E++;
+    return ((mantissa + 1) >> 3) << 1;
   };
 }
 // IF THE FIRST ONE IS IN THE 9 position, then we stop the calculation
@@ -161,12 +169,21 @@ SFP16 fpadd(SFP16 x, SFP16 y)
     }
     else return x;
   }
-  else if (!(exp(y) ^ 0x7F)) return y;
+  else if (!(exp(y) ^ 0x7F)) {
+    if(!(exp(x) ^ 0x7F)){
+      if (sign(x) ^ sign(y))
+          // Inf - Inf or -Inf + Inf
+          return (SFP16) 0x7f01;
+    }
+    else return y;
+  }
   
+
   // catch zeros
   if(!exp(x) && !mantissa(x)){
     return y;
-  }else if(!exp(y) && !mantissa(y)){
+  }
+  if(!exp(y) && !mantissa(y)){
     return x;
   }
 
@@ -227,9 +244,10 @@ SFP16 fpadd(SFP16 x, SFP16 y)
   // printf("y_normal: %hu\n", y_normal);
   
   //Shift Right d places
-  m_y = shift_right_mantissa(m_y, exp(x) - exp(y));
+  m_y = shift_right_mantissa(m_y, E_x - E_y);
   E_y += (exp(x) - exp(y));
-  //printf("E_x: %hu\n", E_x);
+  // printf("m_y: %hu\n", m_y);
+  // printf("d: %hu\n", exp(x) - exp(y));
   
   E = E_x;
   // printf("E: %hu\n", E);
@@ -247,6 +265,11 @@ SFP16 fpadd(SFP16 x, SFP16 y)
       if(x_normal && y_normal){
         res_sign = sign(x);
         res_mantissa = m_y + m_x;
+        // CATCH ZEROS, INF and NAN
+        if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+        
+        
         // printf("m_x: %hu\n", m_x);
         // printf("m_y: %hu\n", m_y);
         // printf("E: %hu\n", E);
@@ -276,73 +299,130 @@ SFP16 fpadd(SFP16 x, SFP16 y)
         // Eliminate first one
         res_mantissa -= 256;
         res_exp = (SFP16) E;
+        // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        // if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
         // printf("res_exp: %hu", res_exp);
       }
-      else{
-      res_sign = sign(x);
-      res_mantissa = m_y + m_x;
-      
-      // res_mantissa is of the shape 10.xxxxx
-      if((res_mantissa > 4096)){
+      else if (x_normal && y_denormal){
+        res_sign = sign(x);
+        res_mantissa = m_y + m_x;
+        if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+        // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        // printf("m_x: %hu\n", m_x);
+        // printf("m_y: %hu\n", m_y);
+        // res_mantissa is of the shape 10.xxxxx
+        // printf("res_mantissa: %hu\n", res_mantissa);
+        if((res_mantissa > 4096)){
+          res_mantissa = shift_right_mantissa(res_mantissa, 1);
+          E++;
+        }
+        // There is not enough E to normalize (The result is denormal)
+        if ((res_mantissa >> 4) == 0 && 8 > E){
+          res_mantissa = res_mantissa << 1;
+          E--;
+          res_denormal = 1;
+        }
+        // printf("res_mantissa: %hu\n\n", res_mantissa);
+        // Adjustment of Round and Sticky bits
         res_mantissa = shift_right_mantissa(res_mantissa, 1);
-        E++;
-      }
-    
-      // printf("counter: %hu\n", counter);
-      // Adjustment of Round and Sticky bits
-      res_mantissa = shift_right_mantissa(res_mantissa, 1);
-      // printf("res_mantissa: %hu\n", res_mantissa);
-      
-      // 5. ROUNDING
-      // WHILE THE VALUE DOES NOT HAVE JUST 9 bits
+        // printf("res_mantissa: %hu\n", res_mantissa);
+        
+        // 5. ROUNDING
+        // WHILE THE VALUE DOES NOT HAVE JUST 9 bits
+
+          res_mantissa = round_mantissa(res_mantissa, 9);
+          if(res_mantissa > 4096){
+            res_mantissa = shift_right_mantissa(res_mantissa, 1);
+            E++;
+          }
+
+        //printf("rounded: %hu\n", res_mantissa);
+        if(res_mantissa >> 8 == 1){
+          res_mantissa -= 256;
+        }
+        //E += BIAS;
+
+        res_exp = (SFP16) E;
+        // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        // if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+        //printf("res_exp: %hu", res_exp);
+        } // DENORMAL + DENORMAL
+        else{
+          res_sign = sign(x);
+          res_mantissa = m_y + m_x;
+          if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+          if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+          // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+          // printf("m_x: %hu\n", m_x);
+          // printf("m_y: %hu\n", m_y);
+          // res_mantissa is of the shape 10.xxxxx
+          // printf("res_mantissa: %hu\n", res_mantissa);
+          if((res_mantissa > 4096)){
+            res_mantissa = shift_right_mantissa(res_mantissa, 1);
+            E++;
+          }
+          // printf("E: %hu\n", E);
+          // There is not enough E to normalize (The result is denormal)
+          if ((res_mantissa >> 4) == 0 && 8 > E ){
+            res_mantissa = res_mantissa << 1;
+
+            E--;
+
+            res_denormal = 1;
+          }
+
+          // printf("res_mantissa: %hu\n\n", res_mantissa);
+          // Adjustment of Round and Sticky bits
+          res_mantissa = shift_right_mantissa(res_mantissa, 1);
+          // printf("\nres_mantissa: %hu\n", res_mantissa);
+          
+          // 5. ROUNDING
+          // WHILE THE VALUE DOES NOT HAVE JUST 9 bits
+            // printf("E: %hu\n", E);
+            res_mantissa = round_mantissa(res_mantissa, 9);
+            // printf("E: %hu\n", E);
+            if(res_mantissa > 4096){
+              res_mantissa = shift_right_mantissa(res_mantissa, 1);
+              E++;
+            }
+
+            //printf("rounded: %hu\n", res_mantissa);
+            if(res_mantissa >> 8 == 1){
+              res_mantissa -= 256;
+              res_normal = 1;
+            }
+            //E += BIAS;
+            // printf("rounded: %hu\n", res_mantissa);
+            
+            res_exp = E;
+            //if(res_exp == 0 && res_mantissa == 0) return 0;
+            if(res_exp == 1 && !res_normal) res_exp = 0;
+            if(res_exp == 0 && res_mantissa == 0) res_exp = 1;
 
 
 
-      // Find the first one
-      SFP16 counting = until_first_one(res_mantissa);
-
-      // Calculate S
-      res_mantissa = res_mantissa << (15 - counting);
-      res_mantissa = shift_right_mantissa(res_mantissa, 16 - (9 + 2));
-      //printf("%hu\n", mantissa);
-  
-      // Round
-      if(res_mantissa % 8 == 0) res_mantissa >> 2;
-      if(res_mantissa % 8 == 1) res_mantissa >> 2;
-      if(res_mantissa % 8 == 2) res_mantissa >> 2;
-      if(res_mantissa % 8 == 3) shift_right_mantissa(res_mantissa, 2);
-      if(res_mantissa % 8 == 4) res_mantissa >> 2;
-      if(res_mantissa % 8 == 5) res_mantissa >> 2;
-      if(res_mantissa % 8 == 6) (shift_right_mantissa(res_mantissa, 3)) << 1;
-      if(res_mantissa % 8 == 7) (res_mantissa + 1)  >> 2;
-
-      res_mantissa >>= 2;
-
-
-
-
-
-
-      //printf("rounded: %hu\n", res_mantissa);
-      res_mantissa -= 256;
-      //E += BIAS;
-
-      res_exp = (SFP16) E;
-      //printf("res_exp: %hu", res_exp);
-      }
+            // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+            // if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+            // printf("E: %hu\n", E);
+            // printf("res_exp: %hu\n", res_exp);
+        }
     }
     // SIGNS ARE DIFFERENT
     else {
       if(x_normal && y_normal){
         res_sign = sign(x);
-        printf("m_x: %hu\n", m_x);
-        printf("m_y: %hu\n", m_y);
+        // printf("m_x: %hu\n", m_x);
+        // printf("m_y: %hu\n", m_y);
 
         res_mantissa = bit_substraction(m_x, m_y);
-        printf("res_mantissa: %hu\n", res_mantissa); // correct
+        if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+        // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        // printf("res_mantissa: %hu\n", res_mantissa);
 
         // First bit after the 1. is also 1
-        if((res_mantissa << 11) % 2 == 1){
+        if(((res_mantissa << 11) >> 15) % 2 == 1){
           E--;
           res_mantissa <<= 1;
         }// There is not enough E to normalize (The result is denormal)
@@ -351,112 +431,127 @@ SFP16 fpadd(SFP16 x, SFP16 y)
           E--;
           res_denormal = 1;
         }
-        printf("res_mantissa: %hu\n", res_mantissa);
+        // printf("res_mantissa: %hu\n", res_mantissa);
         
-        // es_mantissa = res_mantissa << 1;
+        // res_mantissa = res_mantissa << 1;
         // E--;
         // printf("res_mantissa: %hu\n", res_mantissa);
         // Adjustment of Round and Sticky bits
         //if(!res_denormal){
-          res_mantissa = shift_right_mantissa(res_mantissa, 1);
+        res_mantissa = shift_right_mantissa(res_mantissa, 1);
         //}
         // //E--;
-        printf("res_mantissa: %hu\n", res_mantissa);
+        // printf("res_mantissa: %hu\n", res_mantissa);
 
         // 5. ROUNDING
 
         res_mantissa = round_mantissa(res_mantissa, 9);
-        printf("\nres_mantissa: %hu\n", res_mantissa);
+        // printf("\nres_mantissa: %hu\n", res_mantissa);
+        if((res_mantissa > 511)){
+          res_mantissa = shift_right_mantissa(res_mantissa, 1);
+          E++;
+        }
         if(res_mantissa >> 8 == 1){
           res_mantissa -= 256;
         }
         res_exp = (SFP16) E;
 
-        printf("\nres_sign: %hu\n", res_sign);
-        printf("res_exp: %hu\n", res_exp);
-        printf("\nres_mantissa: %hu\n", res_mantissa);
-      }else{
-        res_sign = sign(x);
-        // printf("m_x: %hu\n", m_x);
-        // printf("m_y: %hu\n", m_y);
-
-        res_mantissa = bit_substraction(m_x, m_y);
-
-        SFP16 flag_down = 1;
-        if(res_mantissa < 2048){
-          flag_down = 0;
-        }
-
-        
-        // printf("res_mantissa: %hu\n", res_mantissa);
-        
-
-        // res_mantissa is of the shape 10.xxxxx
-        if((res_mantissa > 4096)){
-          res_mantissa = shift_right_mantissa(res_mantissa, 1);
-          counter++;
-        }
-
-        //printf("res_mantissa: %hu\n\n", res_mantissa);
-        
-
-        // Adjustment of Round and Sticky bits
-        res_mantissa = shift_right_mantissa(res_mantissa, 1);
-        //printf("res_mantissa: %hu\n", res_mantissa);
-        E += counter;
-        // printf("E: %hu\n", E);
-        //E++;
-
-        //printf("res_mantissa: %hu\n", res_mantissa);
-
-        // 5. ROUNDING
-        // SFP16 counter = 0;
-        SFP16 unrounded_mantisa = res_mantissa;
-        // while(1){
-        // res_mantissa = round_mantissa(res_mantissa, 9);
-
-        // Find the first one
-        SFP16 counting = until_first_one(res_mantissa);
-
-        // Calculate S
-        
-        res_mantissa = res_mantissa << (15 - counting);
-        res_mantissa = shift_right_mantissa(res_mantissa, 16 - (9 + 2));
-
-        // printf("\nres_mantissa: %hu\n\n", res_mantissa);
-        
-        //printf("%hu\n", mantissa);
-        if(!flag_down) res_mantissa >>= 1;
-        //printf("\nres_mantissa: %hu\n\n", res_mantissa);
-        // Round
-        if(res_mantissa % 8 == 0) res_mantissa = res_mantissa / 4;
-        if(res_mantissa % 8 == 1) res_mantissa = res_mantissa >> 2;
-        if(res_mantissa % 8 == 2) res_mantissa = res_mantissa >> 2;
-        if(res_mantissa % 8 == 3) res_mantissa = shift_right_mantissa(res_mantissa, 2);
-        if(res_mantissa % 8 == 4) res_mantissa = res_mantissa >> 2;
-        if(res_mantissa % 8 == 5) res_mantissa = res_mantissa >> 2;
-        if(res_mantissa % 8 == 6) res_mantissa = (shift_right_mantissa(res_mantissa, 3)) << 1;
-        
-        
-        if(res_mantissa % 8 ==7) res_mantissa = (res_mantissa + 1) / 4 ;
-
-        //res_mantissa >>= 2;
-
-        //printf("res_mantissa: %hu\n", res_mantissa);
-
-        // if(res_mantissa < 2048){
-        //   res_mantissa = res_mantissa * 2;
-        // }
-
-
-        //printf("rounded: %hu\n", res_mantissa);
-        // res_mantissa = modifyBit(res_mantissa, 9, 0);
-        //E += BIAS;
-        res_exp = (SFP16) E;
-
+        // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        // if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
         // printf("\nres_sign: %hu\n", res_sign);
         // printf("res_exp: %hu\n", res_exp);
+        // printf("\nres_mantissa: %hu\n", res_mantissa);
+      }else if (x_normal && y_denormal){
+        res_sign = sign(x);
+        res_mantissa = bit_substraction(m_x, m_y);
+        if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+        // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+        // printf("m_x: %hu\n", m_x);
+        // printf("m_y: %hu\n", m_y);
+        // res_mantissa is of the shape 10.xxxxx
         // printf("res_mantissa: %hu\n", res_mantissa);
+        if((res_mantissa > 4096)){
+          res_mantissa = shift_right_mantissa(res_mantissa, 1);
+          E++;
+        }
+        // There is not enough E to normalize (The result is denormal)
+        if ((res_mantissa >> 4) == 0 && 8 > E){
+          res_mantissa = res_mantissa << 1;
+          E--;
+          res_denormal = 1;
+        }
+        // printf("res_mantissa: %hu\n\n", res_mantissa);
+        // Adjustment of Round and Sticky bits
+        res_mantissa = shift_right_mantissa(res_mantissa, 1);
+        // printf("res_mantissa: %hu\n", res_mantissa);
+        
+        // 5. ROUNDING
+        // WHILE THE VALUE DOES NOT HAVE JUST 9 bits
+
+          res_mantissa = round_mantissa(res_mantissa, 9);
+          if(res_mantissa > 4096){
+            res_mantissa = shift_right_mantissa(res_mantissa, 1);
+            E++;
+          }
+
+        // printf("rounded: %hu\n", res_mantissa);
+        if(res_mantissa >> 8 == 1){
+          res_mantissa -= 256;
+        }
+
+        res_exp = (SFP16) E;
+        //printf("res_exp: %hu", res_exp);
+        }else{
+          res_sign = sign(x);
+          res_mantissa = bit_substraction(m_x, m_y);
+          if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+          if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+          // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+          printf("m_x: %hu\n", m_x);
+          printf("m_y: %hu\n", m_y);
+          // res_mantissa is of the shape 10.xxxxx
+          printf("res_mantissa: %hu\n", res_mantissa);
+          // catch zero
+          
+          if((res_mantissa > 4096)){
+            res_mantissa = shift_right_mantissa(res_mantissa, 1);
+            E++;
+          }
+          // There is not enough E to normalize (The result is denormal)
+          if ((res_mantissa >> 4) == 0 && 8 > E){
+            res_mantissa = res_mantissa << 1;
+            E--;
+            res_denormal = 1;
+          }
+          printf("res_mantissa: %hu\n\n", res_mantissa);
+          // Adjustment of Round and Sticky bits
+          res_mantissa = shift_right_mantissa(res_mantissa, 1);
+          printf("res_mantissa: %hu\n", res_mantissa);
+          
+          // 5. ROUNDING
+          // WHILE THE VALUE DOES NOT HAVE JUST 9 bits
+
+            res_mantissa = round_mantissa(res_mantissa, 9);
+            if(res_mantissa > 4096){
+              res_mantissa = shift_right_mantissa(res_mantissa, 1);
+              E++;
+            }
+
+            printf("rounded: %hu\n", res_mantissa);
+            if(res_mantissa >> 8 == 1){
+              res_mantissa -= 256;
+            }
+            //E += BIAS;
+
+            res_exp = (SFP16) E;
+            
+            if(res_exp == 1 && !res_normal) res_exp = 0;
+            if(res_exp == 0 && res_mantissa == 0) res_exp = 1;
+            // if(((res_sign == 1) ||(res_sign == 0)) && (res_mantissa == 0) && (res_exp == 0)) return 0;
+            // if (!(res_exp ^ 0x7F) && (res_mantissa ^ 0x00)) return (SFP16) 0x7f01;
+            
+            printf("res_exp: %hu", res_exp);
       }
       
     }
